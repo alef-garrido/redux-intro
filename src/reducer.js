38a9b32df7e73e0f193e4182dd407d3ca98fa044
alef-garrido <oa.g11@icloud.com>
2022-03-28 14:25:05 -0600

    Add example of a Reducer

---
+++ src/reducer.js
@@ -0,0 +1,41 @@
+// state is an arr []
+//Reducer have to be pure funtion, its only dependencies are its argumemnts
+
+// Reducer made with if and else statements
+// let lastid = 0;
+// const reducer = (state = [], action) {
+//   if (action.type === 'bugAdded') 
+//   return [
+//     ...state,
+//     {
+//       id: ++lastid,
+//       description: action.payload.description,
+//       resolvec: false
+//     }
+//   ];
+//   else if (action.type === 'bugedRemoved')
+//     return state.filter(bug => bug.id !== action.payload.if );
+  
+//   return state;
+// }
+
+// Reducer made with switch statement - most common
+
+let lastid = 0;
+const reducer = (state = [], action) {
+  switch(action.type) {
+    case "bugAdded":
+      return [
+            ...state,
+            {
+              id: ++lastid,
+              description: action.payload.description,
+              resolvec: false
+            }
+          ];
+    case "bugRemoved":
+      return state.filter(bug => bug.id !== action.payload.if );
+    default: 
+      return state
+  };
+};
