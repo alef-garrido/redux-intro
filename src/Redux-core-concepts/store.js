df9fd6dc4b8f00eb8361e14879ad8e449d26e4af
alef-garrido <oa.g11@icloud.com>
2022-03-28 17:54:18 -0600

    Add store enhacer to enable Chrome's Redux devTools

--- src/Redux-core-concepts/store.js
+++ src/Redux-core-concepts/store.js
@@ -1,6 +1,6 @@
 import { createStore } from "redux";
 import reducer from './reducer'
 
-const store = createStore(reducer); // createStore is a higher order funtion, here we pass a function reference -not a function call-
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); // createStore is a higher order funtion, here we pass a function reference -not a function call-
 
 export default store;  
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import store from './Redux-core-concepts/store';
+import * as actions from './ReduxFromScratch/actions';
+
+store.subscribe(() => {
+  console.log('Updated!')
+});
+
+store.dispatch(actions.bugAdded('bug 1'));
+store.dispatch(actions.bugAdded('bug 2'));
+store.dispatch(actions.bugAdded('bug 3'));
+store.dispatch(actions.bugResolved(1));
+
+console.log(store.getState());
