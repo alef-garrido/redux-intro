5dee16e8040203b782418de0e6aca451b001b27c
alef-garrido <oa.g11@icloud.com>
2022-03-28 20:24:56 -0600

    Structure files to separate concerns

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ store.subscribe(() => {
   console.log('Updated!')
 });
 
-store.dispatch(actions.bugAdded('bug 1'));
-store.dispatch(actions.bugAdded('bug 2'));
-store.dispatch(actions.bugAdded('bug 3'));
-store.dispatch(actions.bugResolved(1));
+// store.dispatch(actions.bugAdded('bug 1'));
+// store.dispatch(actions.bugAdded('bug 2'));
+// store.dispatch(actions.bugAdded('bug 3'));
+// store.dispatch(actions.bugResolved(1));
 
 console.log(store.getState());
