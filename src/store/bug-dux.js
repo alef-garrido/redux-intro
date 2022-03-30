0929774a98bd33a1f7754e27900ba47bb907d7d4
alef-garrido <oa.g11@icloud.com>
2022-03-30 14:37:39 -0600

    Refactor bug-dux code to use redux-toolkit's slice utility

--- src/store/bug-dux.js
+++ src/store/bug-dux.js
@@ -1,26 +1,27 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createAction, createReducer, createSlice } from "@reduxjs/toolkit";
 
-//Action creators - this function now return action creator functions
-export const bugAdded = createAction("BUG_ADDED");
-export const bugRemoved = createAction("BUG_REMOVED");
-export const bugResolved = createAction("BUG_RESOLVED");
-
-// Reducer
 let lastId = 0;
-
-export default createReducer([], {
-  [bugAdded.type]: (state, action) => {
-    state.push({
-      id: ++lastId,
-      description: action.payload.description,
-      resolved: false,
-    });
-  },
-  [bugRemoved.type]: (state, action) => {
-    state.filter((bug) => bug.id !== action.type.id);
-  },
-  [bugResolved.type]: (state, action) => {
-    const index = state.findIndex((bug) => bug.id === action.payload.id);
-    state[index].resolved = true;
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    bugAdded: (state, action) => {
+      state.push({
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
+      });
+    },
+    bugRemoved: (state, action) => {
+      state.filter((bug) => bug.id !== action.type.id);
+    },
+    bugResolved: (state, action) => {
+      const index = state.findIndex((bug) => bug.id === action.payload.id);
+      state[index].resolved = true;
+    },
   },
 });
+
+export const { bugAdded, bugRemoved, bugResolved } = slice.actions;
+
+export default slice.reducer;
