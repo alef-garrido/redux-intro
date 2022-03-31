b3f08fca64af212f6f0b94bbd2d9ce2f38872de2
alef-garrido <oa.g11@icloud.com>
2022-03-30 21:28:00 -0600

    Added getDefaultMiddleware to replace custom midFunc middleware function

--- src/index.js
+++ src/index.js
@@ -1,31 +1,8 @@
 import configureStore from './store/ConfigureStore';
-import { 
-  bugAdded, 
-  bugResolved, 
-  bugRemoved, 
-  getUnresolvedBugs, 
-  bugAssignedToUser,
-  getBugsByUser
-} from './store/bug-dux';
-import { projectAdded } from './store/projects-dux'; 
-import { userAdded } from './store/users-dux';
  
 const store = configureStore();
 
-store.subscribe(() => {
-  console.log('Updated!')
-});
-
-store.dispatch(userAdded({ name: 'user 1' }));
-// store.dispatch(userAdded({ name: 'user 2' }));
-
-// store.dispatch(projectAdded({ name: "project 1" }));
-// store.dispatch(bugAdded({ description: 'bug 1' }));
-// store.dispatch(bugAdded({ description: 'bug 2' }));
-// store.dispatch(bugAdded({ description: 'bug 3' }));
-// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-// store.dispatch(bugResolved({ id: 1 }));
-
-const unresolvedBugs = getUnresolvedBugs(store.getState())
-const bugs = getBugsByUser(1)(store.getState());
-
+store.dispatch((dispatch, getState) => {
+  dispatch({ type: "bugsReceived", bugs:[1,2,3] })
+  console.log(getState())
+})
--- src/store/middleware/logger.js
+++ src/store/middleware/logger.js
@@ -1,7 +1,5 @@
 const logger = param => store => next => action => {
-  console.log("store", store);
-  console.log("next", next);
-  console.log("action", action);
+  console.log("Logging", param);
   next(action);
 };
 
